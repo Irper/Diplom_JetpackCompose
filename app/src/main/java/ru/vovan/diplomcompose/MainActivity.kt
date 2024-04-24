0baf1c1a6baab18f079f7110fe974f5bf4e70c82
irper <irper02@bk.ru>
2024-04-24 18:48:48 +1000

    Изменение lessons

--- app/src/main/java/ru/vovan/diplomcompose/MainActivity.kt
+++ app/src/main/java/ru/vovan/diplomcompose/MainActivity.kt
@@ -11,6 +11,7 @@ import ru.vovan.diplomcompose.ui.theme.DiplomComposeTheme
 var lessons = listOf<Lesson>(
     Lesson(1, "Механика", listOf("БО941САП"), "Лекция", "Владимир Владимирович Лагойда" ),
     Lesson(2, "Web-программирование", listOf("БО941САП", "БО941ПРИ"), "Лекция", "Кузнецов И.В." ),
+    Lesson(3, "Управление проектами в сфере информационных технологий", listOf("БО941САП", "БО941ПРИ", "БО941ПИА", "БО941БИО", "БО941ГИО" ), "Лекция", "Белозерова С.И." )
 )
 
 class MainActivity : ComponentActivity() {
