2aeeabaceb41aae43b595326b7104421b2026ed6
irper <irper02@bk.ru>
2024-05-05 22:51:24 +1000

    Добавление анимаций

--- app/src/main/java/ru/vovan/diplomcompose/settingscreen/SettingScreen.kt
+++ app/src/main/java/ru/vovan/diplomcompose/settingscreen/SettingScreen.kt
@@ -1,13 +1,21 @@
 package ru.vovan.diplomcompose.settingscreen
 
+import android.annotation.SuppressLint
 import android.app.Activity
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -33,8 +41,10 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import ru.vovan.diplomcompose.R
 import ru.vovan.diplomcompose.component.GradientButton
 import ru.vovan.diplomcompose.component.NumberOfAudience
@@ -67,46 +77,87 @@ fun SettingScreen(){
         Spacer(Modifier.height(16.dp))
     }
 }
+@SuppressLint("UnusedContentLambdaTargetStateParameter")
 @Composable
 fun Password(onClick : () -> Unit){
     val correctPass : String = "123"
     var pass by remember { mutableStateOf("") }
     var passVisibility by remember { mutableStateOf(false) }
+    var enteredIncorrectPass by remember { mutableStateOf(false) }
+    var enteredIncorrectPassCount by remember { mutableStateOf(0) }
 
     val icon = if (passVisibility) painterResource(id = R.drawable.icon_ic_visibility_icon)
     else painterResource(id = R.drawable.icon_ic_off_visibility_icon)
 
-    OutlinedTextField(
+    Box (
         modifier = Modifier
-            .border(
-            width = 3.dp,
-            brush = Brush.horizontalGradient(colors = listOf(mainColor_dark, mainColor_light)),
-            shape = RoundedCornerShape(4.dp)
+            .fillMaxWidth()
+            .heightIn(min = 80.dp),
+    ){
+        OutlinedTextField(
+            modifier = Modifier.align(alignment = Alignment.TopCenter)
+                .border(
+                    width = 3.dp,
+                    brush = Brush.horizontalGradient(colors = listOf(mainColor_dark, mainColor_light)),
+                    shape = RoundedCornerShape(4.dp)
+                )
+                .widthIn(min = 300.dp, max = 500.dp),
+            value = pass,
+            onValueChange = { pass = it },
+            placeholder = { Text(text = "Введите пароль") },
+            visualTransformation = if (passVisibility) VisualTransformation.None
+            else PasswordVisualTransformation(),
+            trailingIcon = {
+                IconButton(onClick = { passVisibility = !passVisibility }) {
+                    Icon(
+                        painter = icon,
+                        contentDescription = null,
+                        modifier = Modifier.size(32.dp)
+                    )
+                }
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            singleLine = true
         )
-            .widthIn(min = 300.dp, max = 500.dp),
-        value = pass,
-        onValueChange = { pass = it },
-        placeholder = { Text(text = "Введите пароль") },
-        visualTransformation = if(passVisibility) VisualTransformation.None
-        else PasswordVisualTransformation(),
-        trailingIcon = {
-            IconButton(onClick = { passVisibility = !passVisibility }) {
-                Icon(
-                    painter = icon,
-                    contentDescription = null,
-                    modifier = Modifier.size(32.dp)
+        if (enteredIncorrectPass){
+            AnimatedContent(
+                modifier = Modifier.align(alignment = Alignment.BottomCenter),
+                targetState = enteredIncorrectPassCount,
+                transitionSpec = {
+                    slideInHorizontally(
+                        animationSpec = tween(200),
+                        initialOffsetX = { fullWidth -> fullWidth }
+                    ) togetherWith
+                            slideOutHorizontally(
+                                animationSpec = tween(200),
+                                targetOffsetX = { fullWidth -> -fullWidth }
+                            )
+                },
+                label = "effect"
+            ) {
+                Text(
+                    text = stringResource(id = R.string.label_incorrect),
+                    textAlign = TextAlign.Center,
+                    fontSize = 14.sp,
+                    color = Color.Red
                 )
             }
-        },
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-        singleLine = true
-    )
+        }
+    }
+
     ButtonSetting (
         modifier = Modifier.width(200.dp),
         text = stringResource(id = R.string.button_enter),
-        onClick = { if(correctPass == pass){ onClick() } }
+        onClick = {
+            if(correctPass == pass)
+                onClick()
+            else
+                enteredIncorrectPass = true
+                enteredIncorrectPassCount++
+        }
     )
 }
+
 @Composable
 fun PassEntered(){
     // Выход из приложения
