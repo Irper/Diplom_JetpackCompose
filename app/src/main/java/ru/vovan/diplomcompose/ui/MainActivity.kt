c8b59e160f57effc28a495a07af32f7f3ff04f32
irper <irper02@bk.ru>
2024-06-19 12:24:18 +1000

    Реорганизация кода для использования на планшетах

--- app/src/main/java/ru/vovan/diplomcompose/ui/MainActivity.kt
+++ app/src/main/java/ru/vovan/diplomcompose/ui/MainActivity.kt
@@ -43,17 +43,17 @@ var listMapPoint = listOf(  MapPoint("3430", LatLng(48.49434065442388, 135.06084
                             MapPoint("3252", LatLng(48.49369542367654, 135.06049504935913)),
                             MapPoint("3132", LatLng(48.49338435909059, 135.06015977325265))
 )
-var listLesson = listOf(    Lesson(audienceId = "3430", numberOfLesson = 1, itemName =  "Механика", date = "21.06.2024", group =  "Группа БО941САП", itemType =  "Лекция", lecturer =  "Кузнецов И.В."),
-                            Lesson(audienceId = "3430", numberOfLesson = 2, itemName =  "Механика", date = "21.06.2024", group =  "Группа БО941САП", itemType =  "Лекция", lecturer =  "Кузнецов И.В."),
-                            Lesson(audienceId = "3430", numberOfLesson = 3, itemName =  "Механика", date = "21.06.2024", group =  "Группа БО941САП", itemType =  "Лекция", lecturer =  "Кузнецов И.В."),
-                            Lesson(audienceId = "3430", numberOfLesson = 4, itemName =  "Механика", date = "21.06.2024", group =  "Группа БО941САП", itemType =  "Лекция", lecturer =  "Кузнецов И.В."),
-                            Lesson(audienceId = "3430", numberOfLesson = 5, itemName =  "Механика", date = "21.06.2024", group =  "Группа БО941САП", itemType =  "Лекция", lecturer =  "Кузнецов И.В."),
-                            Lesson(audienceId = "3430", numberOfLesson = 6, itemName =  "Механика", date = "21.06.2024", group =  "Группа БО941САП", itemType =  "Лекция", lecturer =  "Кузнецов И.В."),
+var listLesson = listOf(    Lesson(audienceId = "3430", numberOfLesson = 1, itemName =  "Java-программирование", date = "21.06.2024", group =  "Группа БО921ПИА", itemType =  "Лабораторные работы", lecturer =  "Исаев М.С."),
+                            Lesson(audienceId = "3430", numberOfLesson = 2, itemName =  "Java-программирование", date = "21.06.2024", group =  "Группа БО921ПРИ", itemType =  "Лабораторные работы", lecturer =  "Исаев М.С."),
+                            Lesson(audienceId = "3430", numberOfLesson = 3, itemName =  "Программная инженерия", date = "21.06.2024", group =  "Группа БО941САП", itemType =  "Лекция", lecturer =  "Кузнецов И.В."),
+                            Lesson(audienceId = "3430", numberOfLesson = 4, itemName =  "Лингвистическое и программное обеспечение", date = "21.06.2024", group =  "Группа БО941САП", itemType =  "Лабораторные работы", lecturer =  "Тимош П.С."),
+                            Lesson(audienceId = "3430", numberOfLesson = 5, itemName =  "Языки и методы программирования", date = "21.06.2024", group =  "Группа БО911ПИА", itemType =  "Лекция", lecturer =  "Кузнецов И.В."),
+                            Lesson(audienceId = "3430", numberOfLesson = 6, itemName =  "Языки и методы программирования", date = "21.06.2024", group =  "Группа БО911ПИА", itemType =  "Лабораторные работы", lecturer =  "Кузнецов И.В."),
 
-                            Lesson(audienceId = "3252", numberOfLesson = 1, itemName =  "Механика", date = "21.06.2024", group =  "Группа БО941САП", itemType =  "Лекция", lecturer =  "Кузнецов И.В."),
-                            Lesson(audienceId = "3252", numberOfLesson = 4, itemName =  "Механика", date = "21.06.2024", group =  "Группа БО941САП", itemType =  "Лекция", lecturer =  "Кузнецов И.В."),
-                            Lesson(audienceId = "3252", numberOfLesson = 5, itemName =  "Механика", date = "21.06.2024", group =  "Группа БО941САП", itemType =  "Лекция", lecturer =  "Кузнецов И.В."),
-                            Lesson(audienceId = "3252", numberOfLesson = 6, itemName =  "Механика", date = "21.06.2024", group =  "Группа БО941САП", itemType =  "Лекция", lecturer =  "Кузнецов И.В."),
+                            Lesson(audienceId = "3252", numberOfLesson = 1, itemName =  "Метрология, стандартизация и сертификация", date = "21.06.2024", group =  "Группа БО931САП", itemType =  "Лекция", lecturer =  "Белоус Т.В."),
+                            Lesson(audienceId = "3252", numberOfLesson = 4, itemName =  "Иностранный язык в профессиональной деятельности", date = "21.06.2024", group =  "Группа БО921ИСГ", itemType =  "Лекция", lecturer =  "Алексеева В.В."),
+                            Lesson(audienceId = "3252", numberOfLesson = 5, itemName =  "Тайм-менеджмент", date = "21.06.2024", group =  "Группы БО931ПИА, БО931НГД", itemType =  "Лекция", lecturer =  "Щуковская М.А."),
+                            Lesson(audienceId = "3252", numberOfLesson = 6, itemName =  "Тайм-менеджмент", date = "21.06.2024", group =  "Группы БО931САП, БО931ПРИ, БО931ПМИ", itemType =  "Лекция", lecturer =  "Щуковская М.А."),
 )
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
